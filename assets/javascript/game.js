b45ec8f385207b8b268a6e87bdab70e30845b1a7
liliana Quinley <lilicuellar99@gmail.com>
2018-09-07 00:38:49 -0600

    Functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,10 @@ console.log ("hola")
 var wordlist = ["arbol", "sombrero", "casa", "carro", "bicicleta"];
 var guessesleft = 12;
 var dash = "";
+var guessedLetter = []
 
+
+// DRAW A DASH FOR THE MISSING LETTER 
 
 var pickaWord = wordlist[Math.floor(Math.random() * wordlist.length)]
 console.log("choosen word is "+ pickaWord)
@@ -19,3 +22,46 @@ for (var i=0; i < pickaWord.length ; i = i+1) {
 document.getElementById("dash").innerHTML = dash; 
 
 console.log("This is the end " + dash)
+
+// LISTEN THE KEY PRESSED
+
+// If the guessedLetter is not in the array then substract and type letter.
+// If the guessedLetter is already in the array then no type. 
+// COUNTING DOWN THE letters guessed
+
+document.onkeypress = function(e) { 
+
+    if (!guessedLetter.includes(e.key)) {
+        guessesleft = guessesleft -1;
+        console.log(e.key);
+        console.log(guessesleft);
+        document.getElementById("countDown").innerHTML = guessesleft;
+    
+        guessedLetter[guessedLetter.length]=e.key;
+        console.log(guessedLetter);
+        document.getElementById("GuessedLetters").innerHTML = guessedLetter;
+
+        if (pickaWord.includes(e.key)) {
+            console.log("is included in word " + e.key);
+            console.log(pickaWord);
+            document.getElementById("word").innerHTML = pickaWord;
+        }
+    }      
+}
+
+
+
+//     document.onkeypressl = function(e) { 
+//     console.log(e.key);
+//     guessedLetter[guessedLetter.length]=e.key;
+//     console.log(guessedLetter)
+//     document.getElementById("GuessedLetters").innerHTML = guessedLetter;
+
+// }
+
+
+
+
+
+
+
